91a64d8611549fc4be1247c720832ae882fef362
taitran123 <tai.troc.snake@gmail.com>
2020-09-17 09:23:56 +0700

    Update 1-Install-cuda.sh

--- 1-Install-cuda.sh
+++ 1-Install-cuda.sh
@@ -4,3 +4,6 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
 sudo apt-get -y install cuda
+sudo reboot
+
+#
